668bcf43c5b9cee6cc1e0e5e1f3c15372d9e3ff3
Ivo Woltring <webmaster@ivonet.nl>
2016-03-14 23:31:20 +0100

    Added KepubStrategy

---
+++ src/main/java/nl/ivonet/epub/strategy/epub/KepubStrategy.java
@@ -0,0 +1,79 @@
+/*
+ * Copyright (c) 2016 Ivo Woltring
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package nl.ivonet.epub.strategy.epub;
+
+import nl.ivonet.epub.annotation.ConcreteEpubStrategy;
+import nl.ivonet.epub.domain.Dropout;
+import nl.ivonet.epub.domain.Epub;
+import nl.siegmann.epublib.domain.Resource;
+import org.apache.commons.io.IOUtils;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+
+/**
+ * @author Ivo Woltring
+ */
+@ConcreteEpubStrategy
+public class KepubStrategy implements EpubStrategy {
+    private static final Pattern PAT = Pattern.compile("<(h\\d|p)([^>]*)>",
+                                                       Pattern.CASE_INSENSITIVE);
+    private static final String KOBO = " id=\"kobo.%s.1\">";
+
+    @Override
+    public void execute(final Epub epub) {
+        final List<Resource> htmlResources = epub.getContents()
+                                           .stream()
+                                           .filter(this::isHtml)
+                                           .collect(Collectors.toList());
+
+        int idx = 1;
+        for (final Resource content : htmlResources) {
+            try {
+                final String html = IOUtils.toString(content.getReader());
+                final Matcher matcher = PAT.matcher(html);
+                final StringBuffer sb = new StringBuffer();
+                while (matcher.find()) {
+                    final String group = matcher.group();
+                    if (doProcess(group)) {
+                        matcher.appendReplacement(sb, group.replace(">", String.format(KOBO, idx)));
+                        idx++;
+                    }
+                }
+                matcher.appendTail(sb);
+                content.setData(sb.toString().getBytes());
+
+            } catch (IOException e) {
+                epub.addDropout(Dropout.READ_ERROR);
+            }
+
+        }
+    }
+
+    private boolean doProcess(final String group) {
+        return !group.contains("id=");
+    }
+
+
+    private boolean isHtml(final Resource content) {
+        return "application/xhtml+xml".equals(content.getMediaType()
+                                                     .toString());
+    }
+}
---
+++ src/test/java/nl/ivonet/epub/strategy/epub/KepubStrategyTest.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright (c) 2016 Ivo Woltring
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package nl.ivonet.epub.strategy.epub;
+
+import nl.ivonet.epub.domain.Epub;
+import nl.ivonet.util.EpubTestUtils;
+import nl.siegmann.epublib.domain.Resource;
+import org.apache.commons.io.IOUtils;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.List;
+
+import static java.util.stream.Collectors.toList;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * @author Ivo Woltring
+ */
+public class KepubStrategyTest {
+    private KepubStrategy strategy;
+    private Epub epub;
+
+    @Before
+    public void setUp() throws Exception {
+        strategy = new KepubStrategy();
+        epub = EpubTestUtils.createTestEpub();
+
+    }
+
+    @Test
+    public void testExecute() throws Exception {
+        strategy.execute(epub);
+        final List<Resource> collect = epub.getContents()
+                                           .stream()
+                                           .filter(p -> p.getMediaType()
+                                                         .toString()
+                                                         .contains("application/xhtml+xml"))
+                                           .collect(toList());
+        assertFalse(collect.isEmpty());
+        final String html = IOUtils.toString(collect.get(1).getReader());
+//        System.out.println("html = " + html);
+        assertTrue(html.contains("<h1 id=\"kobo.1.1\">"));
+        assertTrue(html.contains("<p id=\"kobo.2.1\">"));
+    }
+}
